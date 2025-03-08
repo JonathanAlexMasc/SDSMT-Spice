09f99f14925dae45348b6b41c238947bddce6b23
Jonathan Mascarenhas <mascarenhasjonathan@gmail.com>
2025-03-08 11:36:04 -0700

    fixes

--- Loader.js
+++ Loader.js
@@ -1,8 +1,12 @@
+if (require('electron-squirrel-startup') === true) app.quit();
+
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
 const { exec } = require('child_process');
+
+
 
 let win;
 
